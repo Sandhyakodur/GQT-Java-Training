a558e29cf5d54ebeed81e9b9e9ef47bf66d134bc
sanja <sanja@192.168.1.40>
2023-03-29 20:14:16 +0530

    Patterns - Letter A

---
+++ src/GQTCodes/Patterns2.java
@@ -0,0 +1,26 @@
+package GQTCodes;
+import java.util.Scanner;
+public class Patterns2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println(" Enter the value: ");
+		int n=sc.nextInt();
+		for(int i=0;i<=n-1;i++) {
+			for(int j=0;j<=n-1;j++) {
+				if(j==0 || j==n/2 ||
+						(i==n/2 && j<=n/2) ||
+						(i==0 && j<=n/2)) {
+					System.out.print("$ ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
